8da5fdcb0f365a93563a7c52f4057c58d899ac76
Alan Vardy <alanvardy@gmail.com>
2019-02-05 13:32:48 -0700

    wip

--- main.js
+++ main.js
@@ -1,28 +1,35 @@
-const box = document.querySelector("#box")
-const button = document.querySelector("#new")
-button.addEventListener("click", function() {
-    let newBook = prompt('What is the title?')
-    myLibrary.push(newBook)
-    displayBooks()
+const box = document.querySelector('#box')
+const button = document.querySelector('#new')
+const buttons = []
+button.addEventListener('click', function () {
+  let newBook = prompt('What is the title?')
+  myLibrary.push(newBook)
+  displayBooks()
 })
 
-let myLibrary = ['asdf', 'asdfea']
+let myLibrary = []
 
-function Book(title) {
-    self.title = title
+function Book (title) {
+  self.title = title
 }
 
-function addBookToLibrary() {
+function addBookToLibrary () {
 
 }
 
-function displayBooks() {
-    let books = ''
-    for (let index = 0; index < myLibrary.length; index++) {
-        books += myLibrary[index];
-        books += '<br>'
-        box.innerHTML = books
-    }
+function displayBooks () {
+  let books = ''
+  for (let index = 0; index < myLibrary.length; index++) {
+    books += `<p> ${myLibrary[index]}<button id="b${index}">Delete</button></p>`
+  }
+  box.innerHTML = books
+  for (let index = 0; index < myLibrary.length; index++) {
+    buttons.push(document.querySelector(`#b${index}`))
+    buttons[buttons.length - 1].addEventListener('click', function () {
+      myLibrary.splice(index, 1)
+      displayBooks()
+    })
+  }
 }
 
 displayBooks()
